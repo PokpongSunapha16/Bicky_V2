2bd56c6f8323df404f57fa8e0eeb2e3376ead165
Winterzilla <126278308+PokpongSunapha16@users.noreply.github.com>
2024-12-24 22:53:46 +0700

    Delete dashboard_app directory

--- dashboard_app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class dash(models.Model):
-    date = models.DateField()
-    average = models.FloatField()
-
-    class Meta:
-        ordering = ('date',)
