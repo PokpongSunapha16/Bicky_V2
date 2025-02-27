43851908746f09647c644d61ba0fd8396f35304a
6icsix-ux <supakorn.ra.65@ubu.ac.th>
2025-02-27 15:51:17 +0700

    Clean

--- dashboard_app/decorators.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponseForbidden
-
-def admin_or_seller_required(view_func):
-    def wrapper(request, *args, **kwargs):
-        if not request.user.is_authenticated or not (request.user.is_admin() or request.user.is_seller()):
-            return HttpResponseForbidden("คุณไม่มีสิทธิ์เข้าถึงหน้านี้!")
-        return view_func(request, *args, **kwargs)
-    return wrapper
